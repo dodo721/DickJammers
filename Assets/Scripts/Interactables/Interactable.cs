15ec902104caa8d65ceb1f3d15ce46a2e2c5c910
Lewis Frampton <lewis.frampton@gmail.com>
2020-11-14 12:36:10 +0000

    Added Interactables & Updated Prototyping level

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public bool hasInteracted = false;
+    public bool canInteract = false;
+    public bool canSelect = false;
+
+    public int beesRequired = 0;
+    public int beesConsumed = 0;
+
+    public virtual void UseInteractable()
+    {
+        // Overwrite this for each interactable
+    }
+
+    public virtual void ExtendUpdate()
+    {
+        
+    }
+
+    public virtual void DisplayInteractUI()
+    {
+        // Do later
+    }
+
+    public virtual void RemoveInteractUI()
+    {
+        // Do later
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            DisplayInteractUI();
+            canInteract = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canInteract = false;
+        canSelect = false;
+    }
+
+    protected virtual void Update()
+    {
+        // Check to see if we're in posistion to interact
+        if (canInteract)
+        {
+            // Check to see if the mouse is over the interactable
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.rigidbody != null)
+                {
+                    canSelect = ReferenceEquals(hit.rigidbody.gameObject, gameObject);
+                    Debug.Log("hit ourselves!");
+                }
+                else canSelect = false;
+            }
+            else canSelect = false;
+
+            // If we can select and are wanting to interact with the interactable...
+            if (canSelect && Input.GetKey(KeyCode.Mouse0))
+            {
+                UseInteractable();
+            }
+        }
+
+        ExtendUpdate();
+    }
+}
